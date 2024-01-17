c1d67a92b9c3b42200c6bd8d8b320daf809c7eb0
Stefan <84515587+Stoefza@users.noreply.github.com>
2024-01-17 16:19:36 +0200

    Code tweaks

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,8 +2,8 @@
 // rock = 1
 // paper = 2
 // sciccors = 3
-let scoreUser = 0
-let scoreComputer = 0
+let scoreUser = 0;
+let scoreComputer = 0;
 
 
 
@@ -19,6 +19,8 @@ function getComputerChoice() {
     }
 }
 
+//This function allows for 2 parameters and the then compare them to one another
+// Adjusting the score each time 
 function playRound(playerSelection, computerSelection) {
 
     playerSelection = playerSelection.toLowerCase();
@@ -28,30 +30,32 @@ function playRound(playerSelection, computerSelection) {
         console.log("Its a tie! Please try again.");
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
         scoreComputer++;
-        return "You Lose! Paper beats Rock"
+        return "You Lose! Paper beats Rock";
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         scoreUser++;
-        return "You Win! Rock beats Paper"
+        return "You Win! Rock beats Paper";
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         scoreUser++;
-        return "You Win! Paper beats Rock"
+        return "You Win! Paper beats Rock";
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         scoreComputer++;
-        return "You Lose! Scissors beats Paper"
+        return "You Lose! Scissors beats Paper";
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         scoreUser++;
-        return "You Win! Scissors beats Paper"
+        return "You Win! Scissors beats Paper";
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         scoreComputer++;
-        return "You lose! Rock beats Scissors"
+        return "You lose! Rock beats Scissors";
     }
 }
 
+// This is the main entry point of the game - it will loop over the playRound() funtion 5 times and define the winner at the end of the game
+// It will also ask the user if they would like to play again and restart the game
 function game() {
-    for (let index = 0; index < 5; index++) {
+    for (let roundCount = 0; roundCount < 5; roundCount++) {
         const playerSelection = prompt("Enter your disired weapon.. Rock,Paper or Scissors:");
         const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection))
+        console.log(playRound(playerSelection,computerSelection));
         console.log("Scores: ".concat("You: ", scoreUser, " - ", "Computer: ", scoreComputer));
         
     }
@@ -66,7 +70,7 @@ function game() {
     if (playAgain){
         game();
         } else {
-        alert("Thank you for playing!")
+        alert("Thank you for playing!");
         }
     scoreUser = 0;
     scoreComputer = 0;
